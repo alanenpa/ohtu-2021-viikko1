fbbd747b4246fb87cde80faf99c2a555cf645feb
Patrik Alanen <alanen.patrik@gmail.com>
2021-11-13 20:13:55 +0200

    testaa pylintiä

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         self.tilavuus = max(0, tilavuus)
+        print("asdfghasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfasdfgasdfgasdfgasdfg")
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
